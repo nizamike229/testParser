09bea8ce153df6f9cc1e4d2b923c0b0697e460f9
Nizami <nizamike229@mail.ru>
2024-10-07 17:46:27 +0500

    Add News model

---
+++ Parser/Models/News.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models;
+
+public class News
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime PublishDate { get; set; }
+}
