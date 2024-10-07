ee79992732eec5f6d5d6615b7eb1d6eda373d606
Nizami <nizamike229@mail.ru>
2024-10-07 17:47:03 +0500

    Added IHostedService implementation to parse news during app start

---
+++ Parser/BackgroundServices/MyHostedService.cs
@@ -0,0 +1,24 @@
+﻿using WebApplication1.Interfaces;
+
+namespace WebApplication1.BackgroundServices;
+
+public class MyHostedService : IHostedService
+{
+    private readonly INewsService _newsService;
+
+    public MyHostedService(INewsService newsService)
+    {
+        _newsService = newsService;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _newsService.ParseAndSaveNewsAsync();
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
