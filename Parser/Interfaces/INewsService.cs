e0ce29490e5454d250eae3c0bc179b3b4dea93e0
Nizami <nizamike229@mail.ru>
2024-10-07 17:46:37 +0500

    Added interface to be implemented

---
+++ Parser/Interfaces/INewsService.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Interfaces;
+
+public interface INewsService
+{
+    Task<List<News>> GetNewsByDates(DateTime from, DateTime to);
+    Task<string> GetMostUsedWords();
+    Task<List<News>> GetNewsByWord(string word);
+    Task ParseAndSaveNewsAsync();
+}
