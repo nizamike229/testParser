bf34fce461609154d3894f126f1617e477c4f752
Nizami <nizamike229@mail.ru>
2024-10-08 13:49:19 +0500

    Add DDL

---
+++ Parser/Infrastructure/DDL/public/insert_news.sql
@@ -0,0 +1,12 @@
+﻿create procedure insert_news(IN p_title text, IN p_content text, IN p_date_of_publish timestamp without time zone)
+    language plpgsql
+as
+$$
+BEGIN
+    INSERT INTO news (title, content, date_of_publish)
+    VALUES (p_title, p_content, p_date_of_publish);
+END;
+$$;
+
+alter procedure insert_news(text, text, timestamp) owner to postgres;
+
---
+++ Parser/Infrastructure/DDL/public/news.sql
@@ -0,0 +1,13 @@
+﻿create table news
+(
+    id              serial
+        constraint news_pk
+            primary key,
+    title           varchar(300) not null,
+    date_of_publish timestamp    not null,
+    content         varchar      not null
+);
+
+alter table news
+    owner to postgres;
+
