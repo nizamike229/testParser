645285ebac68e667b642011bedae6d557ba9123d
Nizami <nizamike229@mail.ru>
2024-10-09 17:12:18 +0500

    Wrote custom 'authorize' attribute

---
+++ Parser/CustomAttributes/CustomAuthorizeAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using WebApplication1.Services;
+
+namespace WebApplication1.CustomAttributes;
+
+public class CustomAuthorizeAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var token = context.HttpContext.Session.GetString("auth_token");
+        if (string.IsNullOrEmpty(token))
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+
+        if (!TokenService.IsTokenValid(token))
+        {
+            context.Result = new UnauthorizedResult();
+        }
+    }
+}
