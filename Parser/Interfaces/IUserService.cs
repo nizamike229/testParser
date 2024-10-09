89dc69e974ab1694619f6ee9a9e969932d98d0d9
Nizami <nizamike229@mail.ru>
2024-10-09 17:13:33 +0500

    Add interface to be implemented

---
+++ Parser/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Interfaces;
+
+public interface IUserService
+{
+    Task<string> LoginAsync(User user);
+    Task RegisterAsync(User user); 
+}
